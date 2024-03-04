31c87183f4a243d509bbc055a40b234395728268
co191194 <co191194@outlook.com>
2024-03-04 23:19:47 +0900

    changes wezterm settings
    - add title format setting
    - add status bar setting
    - change font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,8 +1,7 @@
--- return {
--- 	color_scheme = "Catppuccin Mocha",
--- 	window_background_opacity = 0.93,
--- }
-
+-- ウィンドウバータイトルとタブタイトルの設定
+require("format")
+-- ステータスバーの設定
+require("status")
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
@@ -19,7 +18,7 @@ config.window_background_opacity = 0.75
 
 config.default_prog = { "pwsh", "-Login" }
 
-config.font_size = 12.0
+config.font_size = 14.0
 config.font = require("wezterm").font("HackGen Console NF")
 
 config.window_frame = {
@@ -31,6 +30,8 @@ config.disable_default_key_bindings = false
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = require("keybinds").keys
 config.key_tables = require("keybinds").key_tables
+
+config.status_update_interval = 1000
 
 -- and finally, return the configuration to wezterm
 return config
