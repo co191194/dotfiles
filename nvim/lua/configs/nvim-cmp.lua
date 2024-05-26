d269ec9f0d2d760591e65f5dcb6407f902959543
co191194 <co191194@outlook.com>
2024-05-26 18:21:27 +0900

    change nvim setting
    - change keymap for jampable luasnip

--- nvim/lua/configs/nvim-cmp.lua
+++ nvim/lua/configs/nvim-cmp.lua
@@ -33,12 +33,10 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- that way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
+      elseif luasnip.expand_or_locally_jumpable() then
         luasnip.expand_or_jump()
-      -- elseif has_words_before() then
-        -- cmp.complete()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
@@ -46,7 +44,7 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
+      elseif luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
