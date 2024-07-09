0e87dc8e477727df4db42ee8589fe58cdc6fdde0
co191194 <co191194@outlook.com>
2024-07-10 00:21:13 +0900

    add which key setting for nvim

--- nvim/lua/plugins/nvim.lua
+++ nvim/lua/plugins/nvim.lua
@@ -227,15 +227,9 @@ return {
   {
     "folke/which-key.nvim",
     event = "VeryLazy",
-    init = function()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 500
+    config = function()
+      require("configs.which_key")
     end,
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
   },
   {
     "folke/noice.nvim",
