0386d5e4176b7672f0e2d6b32d1c537e49e6a4d3
co191194 <co191194@outlook.com>
2024-12-17 23:52:12 +0900

    show directory path in oil

--- nvim/lua/configs/oil.lua
+++ nvim/lua/configs/oil.lua
@@ -1,3 +1,14 @@
+-- Declare a global function to retrieve the current directory
+function _G.get_oil_winbar()
+  local dir = require("oil").get_current_dir()
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 local detail = false
 require("oil").setup({
   keymaps = {
@@ -12,6 +23,15 @@ require("oil").setup({
         end
       end,
     },
+    ["<M-h>"] = {
+      desc = "Toggle hidden view",
+      callback = function()
+        require("oil").toggle_hidden()
+      end,
+    },
+  },
+  win_options = {
+    winbar = "%!v:lua.get_oil_winbar()",
   },
 })
 
