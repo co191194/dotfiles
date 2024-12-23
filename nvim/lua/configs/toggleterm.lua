593c4ae1b655278c07d205a537e0cffd1a1aa3b9
co191194 <co191194@outlook.com>
2024-12-23 21:21:31 +0900

    add keymap for toggleterm

--- nvim/lua/configs/toggleterm.lua
+++ nvim/lua/configs/toggleterm.lua
@@ -1,20 +1,51 @@
-require("toggleterm").setup({
+local toggleterm = require("toggleterm")
+toggleterm.setup({
   open_mapping = { [[<c-\>]], [[<c-¥>]] },
-  on_open = function ()
+  on_open = function()
     vim.cmd("startinsert")
-  end
+  end,
 })
 
+local wk = require("which-key")
+
 local Terminal = require("toggleterm.terminal").Terminal
-
-local map = function(mode, lhs, rhs, opts)
-  vim.keymap.set(mode, lhs, rhs, opts or {})
-end
-
 local shell = Terminal:new({ hidden = true, direction = "float" })
 
-local opts = { noremap = true, silent = true }
-map({ "n", "t" }, "<M-d>", function()
-  shell:toggle()
-end, opts)
+wk.add({
+  noremap = true,
+  silent = true,
+  mode = { "n", "t" },
+  {
+    "<M-d>",
+    function()
+      shell:toggle()
+    end,
+  },
+  {
+    "<space>to",
+    function ()
+      toggleterm.toggle_all()
+    end
+  }
+})
 
+local function set_terminal_keymap()
+  wk.add({
+    buffer = 0,
+    mode = { "t" },
+    { "<esc>", [[<C-\><C-n>]], desc = "Change Normal Mode" },
+    { "<C-h>", [[<cmd>wincmd h<cr>]], desc = "Move Left Window" },
+    { "<C-j>", [[<cmd>wincmd j<cr>]], desc = "Move Down Window" },
+    { "<C-k>", [[<cmd>wincmd k<cr>]], desc = "Move Up Window" },
+    { "<C-l>", [[<cmd>wincmd l<cr>]], desc = "Move Right Window" },
+    { "<C-w>", [[<C-\><C-n><C-w>]], desc = "WinCmd" },
+  })
+end
+
+vim.api.nvim_create_autocmd({ "TermOpen" }, {
+  group = vim.api.nvim_create_augroup("MyToggleTerm", {}),
+  pattern = "term://*#toggleterm#*",
+  callback = function()
+    set_terminal_keymap()
+  end,
+})
